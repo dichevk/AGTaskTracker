09d738406c386a0136b72a0fdef37199834c020c
KonstantinDichev <kdichev.nl@gmail.com>
2023-08-14 23:25:50 +0200

    tasks loop

--- ag-app/src/app/components/tasks/tasks.component.ts
+++ ag-app/src/app/components/tasks/tasks.component.ts
@@ -1,11 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { TASKS } from 'src/app/data/TaskMockData'; 
+import { ITask } from 'src/app/types/ITask';
 
 @Component({
   selector: 'app-tasks',
   templateUrl: './tasks.component.html',
   styleUrls: ['./tasks.component.css']
 })
-export class TasksComponent {
-  tasks = TASKS
+export class TasksComponent implements OnInit{
+  tasks:ITask[] = TASKS
+
+  constructor(){}
+  
+  ngOnInit():void {
+    
+  }
 }
