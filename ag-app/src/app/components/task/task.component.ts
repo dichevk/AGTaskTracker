0ecdd0538442b1209d8da4a023b294753c66fc18
KonstantinDichev <kdichev.nl@gmail.com>
2023-08-14 23:35:04 +0200

    task component tasks integration

--- ag-app/src/app/components/task/task.component.ts
+++ ag-app/src/app/components/task/task.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit, Input } from '@angular/core';
+import { ITask } from 'src/app/types/ITask';
 @Component({
   selector: 'app-task',
   templateUrl: './task.component.html',
   styleUrls: ['./task.component.css']
 })
-export class TaskComponent {
+export class TaskComponent implements OnInit {
 
+  @Input() task!: ITask
+  constructor(){}
+
+  ngOnInit(): void {
+      
+  }
 }
