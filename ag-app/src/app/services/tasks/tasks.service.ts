74e466493ed8f953874a0f5ab839617431ef5ed9
KonstantinDichev <kdichev.nl@gmail.com>
2023-08-21 23:45:20 +0200

    CRUD task service operations

--- ag-app/src/app/services/tasks/tasks.service.ts
+++ ag-app/src/app/services/tasks/tasks.service.ts
@@ -3,15 +3,43 @@ import { Injectable } from '@angular/core';
 import { ITask } from '../../types/ITask';
 import { TASKS } from '../../data/TaskMockData';
 import { Observable, of } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http'
+
+const httpOptions={
+  headers: new HttpHeaders({
+    'Content-Type': 'application/json',
+  })
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class TasksService {
 
-  constructor() { }
+ 
+  private apiUrl = 'http://localhost:5000/tasks';
+  constructor(private http: HttpClient) {}
 
   getAll():Observable<ITask[]>{
-    return of(TASKS)
+    return this.http.get<ITask[]>(this.apiUrl)
+  }
+
+  getTask(taskId: number): Observable<ITask> {
+    const url = `${this.apiUrl}/${taskId}`;
+    return this.http.get<ITask>(url);
+  }
+
+  updateTask(taskId: number, updatedTask: ITask): Observable<ITask> {
+    const url = `${this.apiUrl}/${taskId}`;
+    return this.http.put<ITask>(url, updatedTask, httpOptions);
+  }
+
+  deleteTask(taskId: number): Observable<void> {
+    const url = `${this.apiUrl}/${taskId}`;
+    return this.http.delete<void>(url);
+  }
+
+  createTask(task:ITask): Observable<ITask>{
+    return this.http.post<ITask>(this.apiUrl, task, httpOptions)
   }
 }
