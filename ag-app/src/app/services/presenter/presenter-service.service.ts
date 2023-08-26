ddab9277291b2758b6012f010179acfcb99bc093
KonstantinDichev <kdichev.nl@gmail.com>
2023-08-26 23:12:24 +0200

    presenter service implementation

--- ag-app/src/app/services/presenter/presenter-service.service.ts
+++ ag-app/src/app/services/presenter/presenter-service.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import { Observable, Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PresenterServiceService {
-
+  private showTask:boolean = false
+  private taskSubject = new Subject()
   constructor() { }
+  toggleAddTask():void {
+    this.showTask = !this.showTask;
+    this.taskSubject.next(this.showTask);
+  }
+  onToggle():Observable<any>{
+    return this.taskSubject.asObservable();
+  }
 }
+
