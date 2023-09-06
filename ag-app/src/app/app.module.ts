184a109bd22aa4fb3a5a5ede51cf7edf733c47ba
KonstantinDichev <kdichev.nl@gmail.com>
2023-09-06 20:12:35 +0200

    add forms module

--- ag-app/src/app/app.module.ts
+++ ag-app/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http'
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
@@ -35,6 +35,7 @@ const appRoutes:Routes = [
     AppRoutingModule,
     FontAwesomeModule,
     HttpClientModule,
+    FormsModule,
     RouterModule.forRoot(appRoutes, {enableTracing:true})
   ],
   providers: [],
